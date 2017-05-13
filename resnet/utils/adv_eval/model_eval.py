2eddad444d56c8f5359cd7dc78791b6bb16ebe41
AtheMathmo <lucasj94@hotmail.co.uk>
2017-05-13 19:25:48 -0400

    Adding adversarial robustness evaluation

---
+++ resnet/utils/adv_eval/model_eval.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+import sys
+import numpy as np
+
+sys.path.append('./cleverhans')
+from cleverhans.attacks_tf import fgsm
+import matplotlib.pyplot as plt
+
+from universal_pert import universal_perturbation
+
+def permute_labels(labels):
+    true_labels = np.argmax(labels, axis=1)
+    adv_labels = (true_labels - 1) % 10
+    one_hot = np.zeros(labels.shape)
+    one_hot[np.arange(labels.shape[0]), adv_labels] = 1.0
+    return one_hot
+
+def fgs_eval(sess, model, data_iter, fgs_eps):
+    '''
+    Returns (untargeted_fgs_acc, targeted_fgs_acc)
+    '''
+    untarget_num_correct = 0.0
+    untarget_count = 0
+    for batch in data_iter:
+        # Try to perturb with cleverhans (untargeted)
+        perturbed_imgs_fgs = sess.run(
+            fgsm(model.input, model.output, fgs_eps, 0.0, 1.0), {
+                model.input: batch["img"]
+        })
+        y = sess.run(model.output, {
+            model.input: perturbed_imgs_fgs
+        })
+        pred_label = np.argmax(y, axis=1)
+        untarget_num_correct += np.sum(np.equal(pred_label, batch["label"]).astype(float))
+        untarget_count += pred_label.size
+    untargeted_pred_acc = (untarget_num_correct / untarget_count)
+
+    # Here we generate targeted adversarial attacks
+    # target_perturbed_imgs_fgs = model.perturb_inputs_fgs(fgs_eps, imgs, target_labels)
+    # untargeted_pred_acc = model.predictive_accuracy(perturbed_imgs_fgs, labels)
+    # targeted_pred_acc = model.predictive_accuracy(target_perturbed_imgs_fgs, target_labels)
+
+    return untargeted_pred_acc
+
+def deepfool_eval(model, imgs, delta=0.2):
+    def f(image_input):
+        return model.sess.run(model.output, {
+            model.input: image_input
+        })
+
+    def f_grad(image_input, inds):
+        return model.sess.run(model.jacobians, {
+            model._input: image_input
+        })
+
+    v = universal_perturbation(imgs, f, f_grad, delta=delta)
+
+def adv_eval(sess, model, data_iter, fgs_eps=0.1):
+    untarget_fgs_acc = fgs_eval(sess, model, data_iter, fgs_eps)
+
+    # Pick some image and plot its true vs the cleverhans adversary
+    # img_idx = 0
+    # all_im = np.concatenate((correct_pred_imgs[img_idx].reshape((32,32,3)),
+    #                          perturbed_imgs_fgs[img_idx].reshape((32,32,3))),
+    #                          axis=1)
+    # plt.imshow(all_im, cmap='gray')
+    # plt.show()
+
+    print("Untargeted FGS pred acc: ", untarget_fgs_acc)
---
+++ run_cifar_adv_eval.py
@@ -0,0 +1,122 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import numpy as np
+import os
+import tensorflow as tf
+
+from tqdm import tqdm
+
+from resnet.configs.cifar_exp_config import get_config, get_config_from_json
+from resnet.data import get_dataset
+from resnet.models import ResNetModel
+from resnet.utils import ExperimentLogger, logger
+from resnet.utils.adv_eval.model_eval import adv_eval
+
+flags = tf.flags
+flags.DEFINE_string("id", None, "eExperiment ID")
+flags.DEFINE_string("dataset", "cifar-10", "Dataset name.")
+flags.DEFINE_string("results", "./results/cifar", "Saving folder")
+flags.DEFINE_string("logs", "./logs/public", "Logging folder")
+FLAGS = tf.flags.FLAGS
+log = logger.get()
+
+def _get_config():
+  # Manually set config.
+  if FLAGS.config is not None:
+    return get_config_from_json(FLAGS.config)
+  else:
+    return get_config(FLAGS.dataset, FLAGS.model)
+
+def get_model(config):
+    with tf.name_scope("Valid"):
+        with tf.variable_scope("Model"):
+            mvalid = ResNetModel(config, is_training=False)
+    return mvalid
+
+def evaluate(sess, model, data_iter):
+    """Runs evaluation."""
+    num_correct = 0.0
+    count = 0
+    iter_ = tqdm(data_iter)
+    for batch in iter_:
+        y = model.infer_step(sess, batch["img"])
+        pred_label = np.argmax(y, axis=1)
+        num_correct += np.sum(np.equal(pred_label, batch["label"]).astype(float))
+        count += pred_label.size
+    acc = (num_correct / count)
+    return acc
+
+def eval_model(config, train_data, test_data, save_folder, logs_folder=None):
+  log.info("Config: {}".format(config.__dict__))
+
+  with tf.Graph().as_default():
+    np.random.seed(0)
+    tf.set_random_seed(1234)
+    exp_logger = ExperimentLogger(logs_folder)
+
+    # Builds models.
+    log.info("Building models")
+    mvalid = get_model(config)
+
+    # # A hack to load compatible models.
+    # variables = tf.global_variables()
+    # names = map(lambda x: x.name, variables)
+    # names = map(lambda x: x.replace("Model/", "Model/Towers/"), names)
+    # names = map(lambda x: x.replace(":0", ""), names)
+    # var_dict = dict(zip(names, variables))
+
+    # Initializes variables.
+    with tf.Session() as sess:
+      # saver = tf.train.Saver(var_dict)
+      saver = tf.train.Saver()
+      ckpt = tf.train.latest_checkpoint(save_folder)
+      # log.fatal(ckpt)
+      saver.restore(sess, ckpt)
+      train_acc = evaluate(sess, mvalid, train_data)
+      val_acc = evaluate(sess, mvalid, test_data)
+
+      # evaluate adversarial robustness
+      test_data.reset()
+      adv_eval(sess, mvalid, test_data)
+
+      niter = int(ckpt.split("-")[-1])
+      exp_logger.log_train_acc(niter, train_acc)
+      exp_logger.log_valid_acc(niter, val_acc)
+    return val_acc
+
+
+def main():
+  config = _get_config()
+  exp_id = FLAGS.id
+
+  save_folder = os.path.realpath(
+      os.path.abspath(os.path.join(FLAGS.results, exp_id)))
+
+  if FLAGS.logs is not None:
+    logs_folder = os.path.realpath(
+        os.path.abspath(os.path.join(FLAGS.logs, exp_id)))
+    if not os.path.exists(logs_folder):
+      os.makedirs(logs_folder)
+  else:
+    logs_folder = None
+
+  # Configures dataset objects.
+  log.info("Building dataset")
+  train_data = get_dataset(
+      FLAGS.dataset,
+      "train")
+  test_data = get_dataset(
+      FLAGS.dataset,
+      "test",
+      cycle=False,
+      data_aug=False,
+      prefetch=False)
+
+  # Evaluates a model.
+  eval_model(config, train_data, test_data, save_folder, logs_folder)
+  adv_eval()
+
+
+if __name__ == "__main__":
+  main()
