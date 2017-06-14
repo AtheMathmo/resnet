ee3f2bef8cb46b2a850c9d940f4ed77ca06913bb
James Lucas <lucasj94@hotmail.co.uk>
2017-06-14 13:58:44 -0400

    Fixing DBP loss bug causing need for large scales

--- resnet/models/resnet_model.py
+++ resnet/models/resnet_model.py
@@ -373,7 +373,7 @@ class ResNetModel(object):
 
   def dbp_loss(self, inputs, labels, logits):
     one_hot_labels = tf.one_hot(labels, self.config.num_classes)
-    ce = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_labels, logits=logits))
+    ce = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_labels, logits=logits))
     loss_deriv = tf.gradients(ce, inputs)[0]
     return tf.reduce_sum(tf.reduce_mean(tf.square(loss_deriv), axis=0))
 
